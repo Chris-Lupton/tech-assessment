3fc7bb36f41fa4134511d3949191a44104697d42
StevenWNorthlink <steven.wilson@northlink.digital>
2023-11-06 11:19:37 +0000

    init

---
+++ app/models/company.server.ts
@@ -0,0 +1,24 @@
+import { prisma } from '~/db.server';
+
+export function getCompany (companyID: string) {
+  return prisma.company.findUniqueOrThrow({
+    where: {
+      id: companyID,
+    },
+  });
+}
+
+export function getCompaniesWithUserId(userId: string) {
+  return prisma.company.findMany({
+    where: {
+      users: {
+        some: {
+          id: userId
+        }
+      }
+    },
+    include: {
+      users: true
+    }
+  });
+}
